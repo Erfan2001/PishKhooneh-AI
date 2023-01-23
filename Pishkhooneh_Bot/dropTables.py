c2e19c504ac6d023e27ecb0a89f7186c299107e7
Erfan <2001erfan1380@gmsil.com>
2023-01-24 02:34:52 +0330

    add multiprocess for bot and flask application

--- Pishkhooneh_Bot/dropTables.py
+++ Pishkhooneh_Bot/dropTables.py
@@ -1,9 +1,9 @@
 import sqlite3
-with sqlite3.connect('db.sqlite3') as connection:
+with sqlite3.connect('Pishkhooneh_Bot\db3.sqlite3') as connection:
     try:
         cur = connection.cursor()
         cur.execute(f'delete from estimate_home')
-        cur.execute(f'delete from estimate_homes2')
+        cur.execute(f'SELECT * from estimate_homes2')
         connection.commit()
     except sqlite3.Error as er:
         print('Total Error => ', er)
--- Pishkhooneh_Bot/main.py
+++ Pishkhooneh_Bot/main.py
@@ -44,7 +44,6 @@ features = {
     'آنتن مرکزی': 'central_antenna',
 }
 def crawler():
-    lastItem="0"
     while 1:
         for epoch in range(0,1):
             print('-------------- Epoch %s Started --------------'%str(epoch+1))
@@ -127,122 +126,104 @@ def crawler():
                     newRecord['graph'] = graphResponse.json()
                 totalData.append(newRecord)
         print(' --- Finished Successfully --- ')
-        haveTotalPrice=[]
+        correctedPrice=[]
         for item in totalData:
-            if item.get('totalPrice'):
-                haveTotalPrice.append(item)
-        haveTotalPrice.reverse()
-        currentIndex=0
-        if lastItem != "0":
-            for item in haveTotalPrice:
-                if str(item.get('identifier'))==lastItem:
-                    currentIndex=haveTotalPrice.index(item)
-        lastItem=str(haveTotalPrice[-1].get('identifier'))
-        if currentIndex != len(haveTotalPrice)-1:
-            newData=haveTotalPrice[currentIndex:]
-        else:
-            newData=[]
-        data=[]
-        for item in newData:
-            if item.get('identifier') in [x.get('identifier') for x in data]:
-                continue
+            if item.get('totalPrice') == None:
+                newItem=item
+                newItem['totalPrice']=0
+                correctedPrice.append(newItem)
             else:
-                data.append(item)
-        totalData.clear()
-        haveTotalPrice.clear()
-        newData.clear()
-        print(currentIndex,'currentIndex',lastItem)
-        # with sqlite3.connect('Pishkhooneh_Bot/db.sqlite3') as connection:
-        #         try:
-        #             cur = connection.cursor()
-        #             rows = cur.fetchall()
-        #         except sqlite3.Error as er:
-        #             print('Total Error => ', er)
+                correctedPrice.append(item)
+        correctedPrice.reverse()
+        data=correctedPrice.copy()
+        correctedPrice.clear()
         print('------------------ Length of total data for process in SQLITE : %s ------------------'%len(data))
         if len(data):
-            with sqlite3.connect('db.sqlite3') as connection:
+            with sqlite3.connect('Pishkhooneh_Bot/db.sqlite3') as connection:
                 try:
 
                         for item in data:
                             cur = connection.cursor()
-                            id1= item.get('identifier')
-                            title=item.get('title')
-                            date=item.get('date')
-                            totalPrice=item.get('totalPrice')
-                            unitPrice=int(totalPrice//item.get('floorArea'))
-                            if item.get('unitPrice'):
-                                unitPrice=item.get('unitPrice')
-                            province=item.get('bread_crumbs')[2]
-                            if item.get('location'):
-                                address=item.get('location').get('locationName')
-                                print(address)
-                            locationX=item.get('location').get('longitude')
-                            locationY=item.get('location').get('latitude')
-                            if len(item.get('images')) <= 1:
-                                if item.get('images')[0].get('photoSmallUrl')=="https://cdn.kilid.com/default-small.png":
-                                    main_image='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'
-                                else:
-                                    main_image=item.get('images')[0].get('photoLargeUrl')
-                                image1=''
-                                image2=''
-                                image3=''
-                            else:
-                                if len(item.get('images')) == 2:
-                                    main_image=item.get('images')[0].get('photoLargeUrl')
-                                    image1=item.get('images')[1].get('photoLargeUrl')
+                            cur.execute('SELECT * FROM estimate_homes2 WHERE id=%s'%item.get('identifier'))
+                            if not cur.fetchone():
+                                id1= item.get('identifier')
+                                title=item.get('title')
+                                date=item.get('date')
+                                totalPrice=item.get('totalPrice')
+                                unitPrice=int(totalPrice//item.get('floorArea'))
+                                if item.get('unitPrice'):
+                                    unitPrice=item.get('unitPrice')
+                                province=item.get('bread_crumbs')[2]
+                                if item.get('location'):
+                                    address=item.get('location').get('locationName')
+                                    print(address)
+                                locationX=item.get('location').get('longitude')
+                                locationY=item.get('location').get('latitude')
+                                if len(item.get('images')) <= 1:
+                                    if item.get('images')[0].get('photoSmallUrl')=="https://cdn.kilid.com/default-small.png":
+                                        main_image='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'
+                                    else:
+                                        main_image=item.get('images')[0].get('photoLargeUrl')
+                                    image1=''
                                     image2=''
                                     image3=''
-                                elif len(item.get('images')) == 3:
-                                    main_image=item.get('images')[0].get('photoLargeUrl')
-                                    image1=item.get('images')[1].get('photoLargeUrl')
-                                    image2=item.get('images')[2].get('photoLargeUrl')
-                                    image3=''
                                 else:
-                                    main_image=item.get('images')[0].get('photoLargeUrl')
-                                    image1=item.get('images')[1].get('photoLargeUrl')
-                                    image2=item.get('images')[2].get('photoLargeUrl')
-                                    image3=item.get('images')[3].get('photoLargeUrl')
-                            type1=item.get('type').get('landuseType')
-                            category=item.get('type').get('propertyType')
-                            status='فروشی'
-                            seller='شخصی'
-                            if item.get('contact'):
-                                if item.get('contact').get('type')=='AGENT':
-                                    seller='شخصی'
-                                elif item.get('contact').get('type')=='AGENCY':
-                                    seller='املاک'
-                            age=0
-                            if item.get('age'):
-                                age=item.get('age')
-                            floorArea=item.get('floorArea')
-                            numOfBeds=1
-                            if item.get('numOfBeds'):
-                                numOfBeds=item.get('numOfBeds')
-                            parking=item.get('features').get('parking')
-                            lobby=item.get('features').get('lobby')
-                            wareHouse=item.get('features').get('warehouse')
-                            sportHall=item.get('features').get('sports_hall')
-                            guard=item.get('features').get('guard')
-                            elevator=item.get('features').get('elevator')
-                            swimmingPool=item.get('features').get('swimming_pool')
-                            balcony=item.get('features').get('balcony')
-                            roofGarden=item.get('features').get('roof_garden')
-                            remoteDoor=item.get('features').get('sports_hall')
-                            description=item.get("description")
-                            description=description.replace('\"','')
-                            region=int(item.get('bread_crumbs')[3].split(' ')[1])
-                            neighbor=item.get('bread_crumbs')[4]
-                            graph=item.get('graph')
-                            phoneNumber='09999999999'
-                            if item.get('contact'):
-                                phoneNumber=item.get('contact').get('callNumber')
-                            isActive=True
-                            owner=''
-                            if item.get('contact'):
-                                owner=item.get('contact').get('fullName')
-                            print('Number => ',data.index(item),' | Identifier:',id1)
-                            cur.execute(f'INSERT INTO estimate_homes2 VALUES ({id1},"{title}",{totalPrice},{unitPrice},"{province}","{address}",{locationX},{locationY},"{main_image}","{image1}","{image2}","{image3}","{type1}","{category}","{status}","{seller}",{age},{floorArea},{numOfBeds},{parking},{lobby},{wareHouse},{sportHall},{guard},{elevator},{swimmingPool},{balcony},{roofGarden},{remoteDoor},"{description}",{region},"{neighbor}","{str(graph)}","{phoneNumber}","کلید","{owner}","{date}","{date}",{isActive})')
-                            connection.commit()
+                                    if len(item.get('images')) == 2:
+                                        main_image=item.get('images')[0].get('photoLargeUrl')
+                                        image1=item.get('images')[1].get('photoLargeUrl')
+                                        image2=''
+                                        image3=''
+                                    elif len(item.get('images')) == 3:
+                                        main_image=item.get('images')[0].get('photoLargeUrl')
+                                        image1=item.get('images')[1].get('photoLargeUrl')
+                                        image2=item.get('images')[2].get('photoLargeUrl')
+                                        image3=''
+                                    else:
+                                        main_image=item.get('images')[0].get('photoLargeUrl')
+                                        image1=item.get('images')[1].get('photoLargeUrl')
+                                        image2=item.get('images')[2].get('photoLargeUrl')
+                                        image3=item.get('images')[3].get('photoLargeUrl')
+                                type1=item.get('type').get('landuseType')
+                                category=item.get('type').get('propertyType')
+                                status='فروشی'
+                                seller='شخصی'
+                                if item.get('contact'):
+                                    if item.get('contact').get('type')=='AGENT':
+                                        seller='شخصی'
+                                    elif item.get('contact').get('type')=='AGENCY':
+                                        seller='املاک'
+                                age=0
+                                if item.get('age'):
+                                    age=item.get('age')
+                                floorArea=item.get('floorArea')
+                                numOfBeds=1
+                                if item.get('numOfBeds'):
+                                    numOfBeds=item.get('numOfBeds')
+                                parking=item.get('features').get('parking')
+                                lobby=item.get('features').get('lobby')
+                                wareHouse=item.get('features').get('warehouse')
+                                sportHall=item.get('features').get('sports_hall')
+                                guard=item.get('features').get('guard')
+                                elevator=item.get('features').get('elevator')
+                                swimmingPool=item.get('features').get('swimming_pool')
+                                balcony=item.get('features').get('balcony')
+                                roofGarden=item.get('features').get('roof_garden')
+                                remoteDoor=item.get('features').get('sports_hall')
+                                description=item.get("description")
+                                description=description.replace('\"','')
+                                region=int(item.get('bread_crumbs')[3].split(' ')[1])
+                                neighbor=item.get('bread_crumbs')[4]
+                                graph=item.get('graph')
+                                phoneNumber='09999999999'
+                                if item.get('contact'):
+                                    phoneNumber=item.get('contact').get('callNumber')
+                                isActive=True
+                                owner=''
+                                if item.get('contact'):
+                                    owner=item.get('contact').get('fullName')
+                                print('Number => ',data.index(item),' | Identifier:',id1)
+                                cur.execute(f'INSERT INTO estimate_homes2 VALUES ({id1},"{title}",{totalPrice},{unitPrice},"{province}","{address}",{locationX},{locationY},"{main_image}","{image1}","{image2}","{image3}","{type1}","{category}","{status}","{seller}",{age},{floorArea},{numOfBeds},{parking},{lobby},{wareHouse},{sportHall},{guard},{elevator},{swimmingPool},{balcony},{roofGarden},{remoteDoor},"{description}",{region},"{neighbor}","{str(graph)}","{phoneNumber}","کلید","{owner}","{date}","{date}",{isActive})')
+                                connection.commit()
                 except sqlite3.Error as er:
                     print('Total Error => ', er)
         data.clear()
@@ -250,5 +231,4 @@ def crawler():
         randomNumber=random.randint(50, 60)
         time.sleep(randomNumber)
         print('----------- Finish Sleep Time : %s  ---------------'%randomNumber)
-# crawler()
 
